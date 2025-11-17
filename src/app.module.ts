90d9cce5225da05d5db1e239f14344a49a7690ab
fabs14 <fabianamnzn14@gmail.com>
2025-11-17 15:46:53 -0400

    db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // Cargar variables de entorno (DATABASE_URL, etc.)
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // Prisma disponible en toda la aplicación
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
